b6d0ea8e131839871d53736f2f06a0e0ae3826e7
D.Coloma <dnl.coloma@gmail.coma>
2018-02-16 00:51:54 +0100

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,26 @@
+require 'open-uri'
 class HomeController < ApplicationController
 
   def homepage
 
+    page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    currencies=page.xpath('//td[2]/a')
+    @list_cur=[]
+    currencies.each do |currency|
+      @list_cur << currency.text
+    end
+    @list_cur.sort!
+
   end
 
   def cours
-    $results = Crypto.new(params[:money][:currency]).get_cours_cryptocurrencies
-    p "=======>"
-    p $results
+    $results = Crypto.new(currency_params).get_cours_cryptocurrencies
+    $currency = currency_params
+    p $currency
+    redirect_to root_path
+  end
+
+  def currency_params
+    params[:money][:currency]
   end
 end
